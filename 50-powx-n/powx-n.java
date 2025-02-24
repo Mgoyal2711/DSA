7297d89abc4eb09c38244d17fcc4c1f7d2b91ead
Mgoyal2711 <165719898+Mgoyal2711@users.noreply.github.com>
2025-02-25 00:19:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (53.12%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,11 @@
 class Solution {
     public double myPow(double x, int n) {
         return Math.pow(x, n);
+        // if(n == 0){
+        //     return 1;
+        // }else if(n < 0){
+        //     return  
+        // }else 
+        // return (x * myPow(x,n-1));
     }
 }
